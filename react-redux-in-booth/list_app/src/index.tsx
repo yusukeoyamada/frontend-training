86493671248a00b99442772caaa555dc0b2d3566
yusukeoyamada <fa19940118@gmail.com>
2019-09-19 19:38:47 +0900

    [react-redux-in-booth][list_app] 環境構築

---
+++ react-redux-in-booth/list_app/src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+
+// import App from './App';
+// import counterReducer, { initialState } from './reducers/reducer';
+import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+import 'semantic-ui-css/semantic.min.css';
+
+// const store = createStore(counterReducer, initialState);
+
+ReactDOM.render(<div>Hello World!!!</div>, document.getElementById(
+  'root',
+) as HTMLElement);
+
+serviceWorker.unregister();
